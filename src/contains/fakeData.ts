6ffee8e8b10a9db2ae80a0bed813f9909094f46d
BlockchainCrazy95 <happyworld0905@gmail.com>
2022-08-27 21:26:43 +0200

    remove images

--- src/contains/fakeData.ts
+++ src/contains/fakeData.ts
@@ -1,13 +1,4 @@
-import avatar1 from "images/avatars/Image-1.png";
-import avatar2 from "images/avatars/Image-2.png";
-import avatar3 from "images/avatars/Image-3.png";
-import avatar4 from "images/avatars/Image-4.png";
-import avatar5 from "images/avatars/Image-5.png";
-import avatar6 from "images/avatars/Image-6.png";
-import avatar7 from "images/avatars/Image-7.png";
-import avatar8 from "images/avatars/Image-8.png";
-import avatar9 from "images/avatars/Image-9.png";
-import avatar10 from "images/avatars/Image-10.png";
+import avatar1 from "images/avatars/default.webp";
 //
 import nfts1 from "images/nfts/1.png";
 import nfts2 from "images/nfts/2.png";
@@ -99,15 +90,15 @@ export const nftsAbstracts = [
 
 export const avatarImgs = [
   avatar1,
-  avatar2,
-  avatar3,
-  avatar4,
-  avatar5,
-  avatar6,
-  avatar7,
-  avatar8,
-  avatar9,
-  avatar10,
+  avatar1,
+  avatar1,
+  avatar1,
+  avatar1,
+  avatar1,
+  avatar1,
+  avatar1,
+  avatar1,
+  avatar1,
 ];
 
 export const personNames = [
